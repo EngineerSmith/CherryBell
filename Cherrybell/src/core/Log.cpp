6b76574e84901044aa954106e2a989b8052e5393
EngineerSmith <pasmith.bus@gmail.com>
2019-10-04 10:32:54 +0400

    Fixed typo in log

--- Cherrybell/src/core/Log.cpp
+++ Cherrybell/src/core/Log.cpp
@@ -9,7 +9,7 @@ namespace CherryBell {
 
 	void Log::Init() {
 		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_coreLogger = spdlog::stdout_color_mt("CHERRYBELL ");
+		s_coreLogger = spdlog::stdout_color_mt("CHERRYBELL");
 		s_coreLogger->set_level(spdlog::level::trace);
 		s_clientLogger = spdlog::stdout_color_mt("APPLICATION");
 		s_clientLogger->set_level(spdlog::level::trace);
