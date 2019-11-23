981310271862ea9f04ecbee08ab3635ad15e5d77
EngineerSmith <pasmith.bus@gmail.com>
2019-11-23 23:40:28 +0400

    Fixed bitwise operator

--- Cherrybell/src/core/Core.h
+++ Cherrybell/src/core/Core.h
@@ -83,7 +83,7 @@ namespace CherryBell {
 		template<class T1, class T2>
 		std::size_t operator() (const std::pair<T1, T2>& pair) const
 		{
-			return std::hash<T1>()(pair.first) & std::hash<T2>()(pair.second);
+			return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
 		}
 	};
 	template<class T1, class T2>
