c23002cb4a9c51343d3ac14eb7ec87267919b045
EngineerSmith <pasmith.bus@gmail.com>
2019-11-23 23:38:31 +0400

    Fixed warning

--- Cherrybell/src/core/debug/Instrumentor.h
+++ Cherrybell/src/core/debug/Instrumentor.h
@@ -112,7 +112,7 @@ namespace CherryBell {
 			long long start = std::chrono::time_point_cast<std::chrono::microseconds>(_startTimepoint).time_since_epoch().count();
 			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
+			uint32_t threadID = (uint32_t)std::hash<std::thread::id>()(std::this_thread::get_id());
 			Instrumentor::Get().WriteProfile({ _name, start, end, threadID });
 
 			_stopped = true;
