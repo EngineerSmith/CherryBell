47211baeb254ac573614c35378900e2ae00def54
Engineer Smith <56016593+EngineerSmith@users.noreply.github.com>
2021-04-25 12:10:55 +0100

    Added missing syntax

--- Cherrybell/src/CherryBell.h
+++ Cherrybell/src/CherryBell.h
@@ -16,7 +16,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#define TO_RAD(degrees) glm::radians(degrees)
+#define TO_RAD(degrees) ::glm::radians(degrees)
 // -------------------------------------
 
 // ----- Debugging ---------------------
